dd0c491b547250eb8927dde652e21fe1c5671c7d
Stoliarchuk-Ilona <76506295+Stoliarchuk-Ilona@users.noreply.github.com>
2021-06-08 03:02:56 +0300

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstWebAPI
+{
+    public class Constants
+    {
+        public static string address = "http://ws.audioscrobbler.com/2.0/";
+        public static string apikey = "18447e7c1d05847e160272fcc9cbf4a7";
+        public static string AccesKey = "AKIA6QTBXK66WLFZTZPH";
+        public static string Secret = "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";
+        public static string TableName = "forAPI-db";
+    }
+}
